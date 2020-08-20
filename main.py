75fca3f455ac123d2ddf387603950d6da7a61b64
carlos <juankarlos.0304@gmail.com>
2020-08-20 15:43:07 -0500

    Terminado del arbol de datos
    Se termino el arbol que mostrara las bases de datos y las tablas que lo componen y con las imagenes de cada uno

--- main.py
+++ main.py
@@ -13,7 +13,10 @@ class main ( wx.Frame ):
 
 	controller = Controller()
 
+	List_tables = []
+
 	is_connect = False
+	database_old_selected = ''
 	
 	def __init__( self, parent ):
 
@@ -40,6 +43,8 @@ class main ( wx.Frame ):
 		
 		self.tree = wx.TreeCtrl( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TR_DEFAULT_STYLE )
 		bSizer1.Add( self.tree, 1, wx.ALL|wx.EXPAND, 5 )
+
+		self.add_items_tree()
 
 		self.notebook = wx.aui.AuiNotebook( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.aui.AUI_NB_DEFAULT_STYLE )
 		self.m_panel1 = wx.Panel( self.notebook, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
@@ -70,6 +75,96 @@ class main ( wx.Frame ):
 		NewBD = Dialog_Connection(self)
 		NewBD.ShowModal()
 		NewBD.Destroy()
+
+	# =====> TREE
+	def add_items_tree(self):
+		root = self.tree.AddRoot('Databases')
+		self.tree.Expand(root)
+		self.get_databases_list(self.tree,root)
+
+	def get_databases_list(self,tree,root):
+		self.items_database_list = {}
+
+		image_list = wx.ImageList(16, 16)
+		img_database = image_list.Add(wx.Image("img/database.png", wx.BITMAP_TYPE_PNG).Scale(16,16).ConvertToBitmap())
+		self.img_table    = image_list.Add(wx.Image("img/table.png", wx.BITMAP_TYPE_PNG).Scale(16,16).ConvertToBitmap())
+
+		tree.AssignImageList(image_list)
+
+		if self.is_connect == True:
+			rows = self.controller.get_databases()
+			for row in rows:
+				item = tree.AppendItem(root, row[0])
+				self.items_database_list[row[0]]=(item)
+				# Agregando imagen a los items				
+				tree.SetPyData(item, None)
+				tree.SetItemImage(item, img_database, wx.TreeItemIcon_Normal)
+				self.Bind(wx.EVT_TREE_SEL_CHANGED, self.OnSelChanged, tree)
+
+	def OnSelChanged(self,event):
+		items =  event.GetItem()
+		db_nodo = self.tree.GetItemText(items)
+
+		if db_nodo != 'Databases':
+			if self.check_is_database(items) == 1:
+				self.database_active = db_nodo
+				#print(db_nodo)
+				if self.database_old_selected == '':
+					self.database_old_selected = db_nodo
+					self.object_item = self.items_database_list.get(db_nodo)
+				else:
+					self.database_old_selected = db_nodo
+					self.tree.DeleteChildren(self.object_item)
+					self.object_item = self.items_database_list.get(db_nodo)
+
+				if self.database_active != '':
+					self.add_tables_nodo(self.items_database_list.get(db_nodo), self.tree.GetItemText(items))
+					#print(self.List_tables)
+				self.option_menu = ['New Table', 'Drop DB']
+			else:
+				self.table_active = db_nodo
+				self.option_menu = ['Select','Edit','Truncate','Add','Drop Table','Describe']
+		else:
+			self.option_menu = ['New DB']
+
+		self.popupmenu = wx.Menu()
+		for text in self.option_menu:
+			item = self.popupmenu.Append(-1, text)
+			self.Bind(wx.EVT_MENU, self.OnPopupItemSelected, item)
+		self.tree.Bind(wx.EVT_CONTEXT_MENU, self.OnShowPopup)
+
+	def check_is_database(self,db):
+		for value in self.items_database_list.values():
+			if value == db:
+				return 1
+		
+	
+	def add_tables_nodo(self,root,db_name):
+		n = len(self.List_tables)
+		if n == 0:
+			pass
+		else:
+			self.List_tables = []
+
+		rows = self.controller.get_tables(self.database_active)
+		for row in rows:
+			w = self.tree.AppendItem(root,str(row[0]))
+			self.List_tables.append(row[0])
+
+			self.tree.SetPyData(w, None)
+			self.tree.SetItemImage(w, self.img_table, wx.TreeItemIcon_Normal)
+	
+	def OnShowPopup(self, event):
+		pos = event.GetPosition()
+		pos = self.ScreenToClient(pos)
+		self.PopupMenu(self.popupmenu, pos)
+
+	def OnPopupItemSelected(self, event):
+		print("=============> OnPopupItemSelected")
+		item = self.popupmenu.GetLabel(event.GetId())
+
+		if item == 'Select':
+			self.notebook.AddPage( select_table_panel(self.notebook, self.database_active, self.table_active), u"Select Table")
 
 
 
@@ -158,8 +253,30 @@ class Dialog_Connection ( wx.Dialog ):
 			else:
 				wx.MessageBox(str(r) , 'Info',wx.OK | wx.ICON_INFORMATION)
 
+class select_table_panel(wx.Panel):
+	"""docstring for select_table_panel"""
 
+	controller = Controller()
 
+	def __init__(self, parent, db, table):
+		wx.Panel.__init__ ( self, parent=parent, id = wx.ID_ANY, pos = wx.DefaultPosition, size = wx.Size( 500,300 ), style = wx.TAB_TRAVERSAL )
+
+		self.table = table
+		self.db = db
+		bSizer12 = wx.BoxSizer( wx.VERTICAL )
+
+		self.tool2 = wx.ToolBar( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TB_HORIZONTAL )
+		self.tool_add_row = self.tool2.AddLabelTool( wx.ID_ANY, u"tool", wx.Bitmap( u"img/png/002-anadir.png", wx.BITMAP_TYPE_ANY ), wx.NullBitmap, wx.ITEM_NORMAL, u"Add", wx.EmptyString, None ) 
+		
+		self.tool2.Realize()
+		
+		bSizer12.Add( self.tool2, 0, wx.EXPAND, 5 )
+
+		id=wx.NewId()
+		self.lista=wx.ListCtrl(self,id,style=wx.LC_REPORT|wx.SUNKEN_BORDER)
+		bSizer12.Add(self.lista, 1, wx.EXPAND)
+		self.SetSizer( bSizer12 )
+		self.Layout()
 
 app = wx.App()
 main(None)
